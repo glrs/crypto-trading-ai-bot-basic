082c7c7c0a0d09e6420826a331442e6b63119dd5
glrs <5999366+glrs@users.noreply.github.com>
2021-03-28 01:55:49 +0100

    remove unnecessary ';' character

--- VirtualAccount.py
+++ VirtualAccount.py
@@ -6,4 +6,4 @@ class VirtualAccount:
         self.btc_balance = 0
         self.btc_price = 0
         self.bought_btc_at = 0
-        self.last_transaction_was_sell = False;
+        self.last_transaction_was_sell = False
